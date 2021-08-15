8b341423b4d3e49bee55637085baadf4823b0a4a
AdheCode <adhep86@gmail.com>
2021-08-15 14:32:43 +0700

    bagian 3-4

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+    return (
+      <div className='center'>
+        <h1>Page not Found!</h1>
+      </div>
+    )
+  }
---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,7 @@
+export default function SelectedClientProjectsPage() {
+    return (
+      <div>
+        <h1>The Project Page for a Specific Project of a Given Client!</h1>
+      </div>
+    )
+  }
